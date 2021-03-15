04bc5dc9536de500d6a4d98c8c3635ed1151ecf6
Subhas Luchmun <subhas.luchmun@gmail.com>
2021-03-15 14:48:35 -0400

    Initial Commit

---
+++ VirtualLab/VirtualLab/BusinessObjects/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VirtualLab.BusinessObjects
+{
+    public class Employee 
+    { 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public string Address { get; set; }
+
+        public string Country { get; set; }
+
+
+    }
+}
